a7d47751e7141076a09235d9c8e7a0151ddd9b54
Zabdiel Reyes <zabreor@gmail.com>
2023-05-08 20:52:54 -0600

    login request improved

--- public/js/HttpService.js
+++ public/js/HttpService.js
@@ -1,59 +1,86 @@
-
 class HttpService {
     constructor(headers = {}) {
-      this.headers = headers;
-      this.baseUrl = window.location.origin;
+        this.headers = headers;
+        this.baseUrl = window.location.origin;
     }
-  
+
     setHeaders(headers) {
-      this.headers = headers;
+        this.headers = headers;
     }
-  
+
     async get(endpoint, queryParams = {}) {
-      const url = new URL(endpoint, this.baseUrl);
-      for (const [key, value] of Object.entries(queryParams)) {
-        url.searchParams.append(key, value);
-      }
-      const response = await fetch(url.toString(), { headers: this.headers });
-      return response.json();
+        const url = new URL(endpoint, this.baseUrl);
+        for (const [key, value] of Object.entries(queryParams)) {
+            url.searchParams.append(key, value);
+        }
+        const response = await fetch(url.toString(), { headers: this.headers });
+        return response.json();
     }
-  
+
     async post(endpoint, data = {}, queryParams = {}) {
-      const url = new URL(endpoint, this.baseUrl);
-      for (const [key, value] of Object.entries(queryParams)) {
-        url.searchParams.append(key, value);
-      }
-      const response = await fetch(url.toString(), {
-        method: 'POST',
-        headers: this.headers,
-        body: JSON.stringify(data),
-      });
-      return response.json();
+        const url = new URL(endpoint, this.baseUrl);
+        for (const [key, value] of Object.entries(queryParams)) {
+            url.searchParams.append(key, value);
+        }
+
+        const headers =
+            Object.keys(this.headers).length === 0
+                ? {
+                      Accept: 'application/json',
+                      'Content-Type': 'application/json',
+                  }
+                : this.headers;
+
+        const response = await fetch(url.toString(), {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data),
+        });
+
+        return response.json();
     }
-  
+
     async put(endpoint, data = {}, queryParams = {}) {
       const url = new URL(endpoint, this.baseUrl);
       for (const [key, value] of Object.entries(queryParams)) {
         url.searchParams.append(key, value);
       }
+    
+      const headers = Object.keys(this.headers).length === 0 ? 
+        {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        } :
+        this.headers;
+    
       const response = await fetch(url.toString(), {
         method: 'PUT',
-        headers: this.headers,
+        headers: headers,
         body: JSON.stringify(data),
       });
+    
       return response.json();
     }
-  
+    
     async delete(endpoint, queryParams = {}) {
       const url = new URL(endpoint, this.baseUrl);
       for (const [key, value] of Object.entries(queryParams)) {
         url.searchParams.append(key, value);
       }
+    
+      const headers = Object.keys(this.headers).length === 0 ? 
+        {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        } :
+        this.headers;
+    
       const response = await fetch(url.toString(), {
         method: 'DELETE',
-        headers: this.headers,
+        headers: headers,
       });
+    
       return response.json();
     }
-  }
-  
+    
+}
--- public/js/requests.js
+++ public/js/requests.js
@@ -1,19 +1,14 @@
+
+const httpService = new HttpService();
+
 const login = async (username, password) => {
     try {
-        const rawResponse = await fetch('/api/auth/login', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
-        const response = await rawResponse.json();
-                
+        const data = { username, password };
+        const response = await httpService.post('/api/auth/login', data);
+
         return response;
     } catch (error) {
         console.info(error);
         return error;
     }
 };
-
