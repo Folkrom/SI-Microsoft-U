80ed086002c563bc36791154ec26eee93fded210
Zabdiel Reyes <zabreor@gmail.com>
2023-05-09 20:05:25 -0600

    admin-panel route added

--- server/routes/navigation.js
+++ server/routes/navigation.js
@@ -2,6 +2,7 @@ import Router from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { validateJWT } from '../middlewares/validate-JWT.js';
+import { isAdminRole } from '../middlewares/validate-roles.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,6 +23,14 @@ router.get('/dashboard', [
     validateJWT
 ], (req, res) => {
     const route = join(mainRoute, 'pages', 'dashboard.html');
+    res.sendFile(route);
+});
+
+router.get('/admin-panel', [
+    validateJWT,
+    isAdminRole
+], (req, res) => {
+    const route = join(mainRoute, 'pages', 'admin-panel.html');
     res.sendFile(route);
 });
 
