002f2c56868579ddde645837b2435797e9c5bff0
Zabdiel Reyes <zabreor@gmail.com>
2023-05-14 22:43:23 -0600

    employees menu added

--- public/js/employees-panel.js
+++ public/js/employees-panel.js
@@ -2,7 +2,7 @@ const loadEmployees = async () => {
     const employeesTable = document.getElementById('employees-table');
     const employeesTbody = employeesTable.querySelector('tbody');
 
-    const { empleados: employees } = await getEmployees();
+    const { employees } = await getEmployees();
 
     employeesTbody.innerHTML = '';
     employees.forEach((employee) => {
