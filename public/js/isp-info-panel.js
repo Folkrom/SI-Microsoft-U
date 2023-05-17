b426b29bd38ad92cf3a2f0f672d9b7dee59faff6
Zabdiel Reyes <zabreor@gmail.com>
2023-05-16 19:07:26 -0600

    isp-info menu added

--- public/js/isp-info-panel.js
+++ public/js/isp-info-panel.js
@@ -20,3 +20,41 @@ const loadISPinfo = async () => {
         ISPinfoTbody.appendChild(row);
     });
 };
+
+const handleSubmit = async (event) => {
+    event.preventDefault();
+    const registerResult = document.getElementById('register-err');
+
+    const nombre = document.getElementById('nombre').value;
+    const tipo_dispositivo = document.getElementById('tipo-dispositivo').value;
+    const tipo_conexion = document.getElementById('tipo-conexion').value;
+    const conexion_maxima = document.getElementById('conexion-maxima').value;
+    const ruta = document.getElementById('ruta').value;
+    const direccion_ip = document.getElementById('direccion-ip').value;
+    const alcance = document.getElementById('alcance').value;
+
+    const formData = {
+        Nombre: nombre,
+        Tipo_de_dispositivo: tipo_dispositivo,
+        Tipo_de_conexion: tipo_conexion,
+        Conexion_maxima: conexion_maxima,
+        Ruta: ruta,
+        Direccion_IP: direccion_ip,
+        Alcance: alcance,
+    };
+
+    const validRegister = await registerData('ISPinfo', formData);
+
+    if (validRegister.err) {
+        registerResult.innerHTML = validRegister.err;
+        return;
+    }
+
+    document.querySelectorAll('input').forEach((input) => {
+        input.value = '';
+    });
+    registerResult.innerHTML = validRegister.msg;
+    registerResult.style = 'color: #00FF00;';
+
+    loadISPinfo();
+};
