b0b27f3a9ade1ab1bccc76b352d98dd41bdc4064
Zabdiel Reyes <zabreor@gmail.com>
2023-06-14 20:30:48 -0600

    added ISP-requests form and now is working!

--- public/js/isp-requests-panel.js
+++ public/js/isp-requests-panel.js
@@ -23,3 +23,46 @@ const loadISPrequests = async () => {
         ISPrequestsTbody.appendChild(row);
     });
 };
+
+const handleSubmit = async (event) => {
+    event.preventDefault();
+    const registerResult = document.getElementById('register-err');
+
+    const autorizacion = document.getElementById('autorizacion').value;
+    const ampliacionAutorizacion = document.getElementById(
+        'ampliacionAutorizacion'
+    ).value;
+    const entidadPrivada = document.getElementById('entidadPrivada').value;
+    const autorizacionPreviaISP = document.getElementById(
+        'autorizacionPreviaISP'
+    ).value;
+    const criterioReferencia =
+        document.getElementById('criterioReferencia').value;
+    const vigencia = document.getElementById('vigencia').value;
+    const alcance = document.getElementById('alcance').value;
+
+    const formData = {
+        Autorizacion: autorizacion,
+        Ampliacion_de_Autorizacion: ampliacionAutorizacion,
+        Entidad_Privada: entidadPrivada,
+        Autorizacion_Previa_del_ISP: autorizacionPreviaISP,
+        Criterio_de_Referencia: criterioReferencia,
+        Vigencia: vigencia,
+        Alcance: alcance,
+    };
+
+    const validRegister = await registerData('ISPRequests', formData);
+
+    if (validRegister.err) {
+        registerResult.innerHTML = validRegister.err;
+        return;
+    }
+
+    document.querySelectorAll('input').forEach((input) => {
+        input.value = '';
+    });
+    registerResult.innerHTML = validRegister.msg;
+    registerResult.style = 'color: #00FF00;';
+
+    loadISPrequests();
+};
